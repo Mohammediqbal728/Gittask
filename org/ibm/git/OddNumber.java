a347e8880f0268d45f0b8f3b270e36da1720d89c
Mohammediqbal728 <mohammediqbal728@gmail.com>
2019-06-29 19:45:31 -0700

    updated

---
+++ org/ibm/git/OddNumber.java
@@ -0,0 +1,19 @@
+package org.ibm.git;
+
+public class OddNumber {
+public static void main(String[] args) {
+	/*for(int i=1;i<=30;i++) {
+		if(i%2==1) {
+			System.out.println(i);
+		}
+		*/
+			int count =0;
+for(int k=0;k<=100;k++) {
+if(k%2==1) {
+	count = count+k;
+}
+}
+System.out.println(count);
+		}
+	}
+
